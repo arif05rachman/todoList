0b7cd0d9da7a1d3af986cf8fe90f431afb9b9e26
Arif Rachman Husen <arif05rachman@gmail.com>
2021-12-31 09:21:37 +0700

    done

--- App.js
+++ App.js
@@ -1,11 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  ScrollView,
+} from "react-native";
+import {} from "react-native-web";
+import Task from "./components/Task";
 
 export default function App() {
+  const [id, setId] = useState(1);
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState({ id: 1, title: "", done: false });
+  const handleAdd = () => {
+    Keyboard.dismiss();
+    setTasks([...tasks, task]);
+    setTask({ ...task, title: "", id: task.id + 1 });
+  };
+
+  const completedTask = (id) => {
+    const doneTasks = [...tasks].map((item) => {
+      if (item.id === id) {
+        return { ...item, done: true };
+      }
+      return item;
+    });
+    setTasks(doneTasks);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskWrapper}>
+        <Text style={styles.titleText}>Today's tasks</Text>
+        <View style={styles.items}>
+          <ScrollView>
+            {tasks?.map((item) => (
+              <TouchableOpacity
+                key={item?.id}
+                onPress={() => completedTask(item?.id)}
+              >
+                <Task task={item} />
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          value={task.title}
+          style={styles.input}
+          onChangeText={(text) => setTask({ ...task, title: text })}
+          placeholder="Write a task"
+        />
+        <TouchableOpacity onPress={() => handleAdd()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -13,8 +72,50 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
   },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  titleText: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+    padding: 20,
+    marginBottom: 180,
+    backgroundColor: "#fff",
+    minWidth: "80%",
+    minHeight: "70%",
+    borderRadius: 10,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 20,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    padding: 15,
+    width: 250,
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "#c0c0c0",
+    borderRadius: 60,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#c0c0c0",
+  },
+  addText: {},
 });
---
+++ components/Task.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const Task = ({ task }) => {
+  console.log(task);
+  return (
+    <View style={task?.done ? styles.itemDone : styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={task?.done ? styles.squareDone : styles.square}></View>
+        <Text
+          style={
+            styles.itemText &&
+            task?.done && { textDecorationLine: "line-through" }
+          }
+        >
+          {task.title}
+        </Text>
+      </View>
+      <View style={task.done ? styles.circularDone : styles.circular}></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#f8f9fa",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemDone: {
+    backgroundColor: "#dee2e6",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  itemLeft: { flexDirection: "row", alignItems: "center", flexWrap: "wrap" },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  squareDone: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#00b4d8",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: "#55BCF6",
+    opacity: 0.4,
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+  circularDone: {
+    width: 12,
+    height: 12,
+    borderColor: "#00b4d8",
+    backgroundColor: "#00b4d8",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
+
+export default Task;
